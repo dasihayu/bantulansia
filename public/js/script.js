74b1e9b87a737a86c9358befc8d92f98449f372b
akmmp241 <akm.mp24@gmail.com>
2022-09-11 12:12:51 +0700

    menambah fitur back to top

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,41 @@
 // Scroll
 let scrollAwal = window.pageYOffset
 window.onscroll = function() {
-    const header = document.querySelector('header');
+    const header = document.querySelector('header')
     let scrollAkhir = window.pageYOffset
+    const toTop = document.querySelector('#to-top')
 
     if (scrollAwal > scrollAkhir) {
-        header.classList.remove('hide-navbar');
+        header.classList.remove('hide-navbar')
         if (window.pageYOffset > 0) {
             header.classList.add('border-b-2')
         } else {
             header.classList.remove('border-b-2')
         }
+        toTop.classList.remove('flex')
+        toTop.classList.add('hidden')
     } else {
-        header.classList.add('hide-navbar');
+        header.classList.add('hide-navbar')
+        toTop.classList.remove('hidden')
+        toTop.classList.add('flex')
     }
 
     scrollAwal = scrollAkhir
-};
+}
 
 // hamburger
-const hamburger = document.querySelector('#hamburger');
+const hamburger = document.querySelector('#hamburger')
 const navMenu = document.querySelector('#nav-menu')
 
 hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
+    hamburger.classList.toggle('hamburger-active')
     navMenu.classList.toggle('hidden')
 });
+
+// tutup hamburger
+window.addEventListener('click', function(e) {
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active')
+        navMenu.classList.add('hidden')
+    }
+})
